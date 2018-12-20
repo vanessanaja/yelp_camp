30ba59fcfc893890abe18e959d732ca5115be35d
Vanessa Naja <vcpn@yahoo.com>
2018-12-20 22:53:53 +0000

    update partials

--- V1/app.js
+++ V1/app.js
@@ -1,27 +1,41 @@
 const express = require("express");
 const app = express();
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+let campgrounds = [
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+       ];
 
 app.get('/', function(req, res){
    res.render('landing'); 
 });
 
 app.get('/campgrounds', function(req, res){
-   var campgrounds = [
-        {name: 'Salmon Creek', image:'https://source.unsplash.com/y8Ngwq34_Ak'},
-        {name: 'Malibu Creek', image:'https://source.unsplash.com/1azAjl8FTnU'},
-        {name: 'Frog Pond', image:'https://source.unsplash.com/eDgUyGu93Yw'}
-       ] 
-        res.render('campgrounds', {campgrounds: campgrounds});
+    res.render('campgrounds', {campgrounds: campgrounds});
 });
 
 app.post('/campgrounds', function(req, res){
-    res.send('you hit the posts route');
+    let name = req.body.name; 
+    let image = req.body.image; 
+    let newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+    res.redirect('/campgrounds');
 });
 
+app.get('/campgrounds/new', function(req, res){
+   res.render('new.ejs'); 
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('Yelp Camp server has started');
