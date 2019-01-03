a15b0190a581b4363202eee00f7756c2844cdfc4
Vanessa Naja <vcpn@yahoo.com>
2019-01-03 23:55:44 +0000

    version 2 initial commit

---
+++ V2/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+let campgrounds = [
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+       ];
+
+app.get('/', function(req, res){
+   res.render('landing'); 
+});
+
+app.get('/campgrounds', function(req, res){
+    res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.post('/campgrounds', function(req, res){
+    let name = req.body.name; 
+    let image = req.body.image; 
+    let newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+    res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', function(req, res){
+   res.render('new.ejs'); 
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Yelp Camp server has started');
+});
