152741ca6546c18529023a7382e3c1532bf6958c
Vanessa Naja <vcpn@yahoo.com>
2019-01-04 00:33:34 +0000

    update app.get for campgrounds page

--- V2/app.js
+++ V2/app.js
@@ -15,36 +15,32 @@ const campgroundSchema = new mongoose.Schema({
 
 const Campground = mongoose.model("Campground", campgroundSchema); 
 
-Campground.create({
-    name: "Salmon Creek", 
-    image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"
-}, function(err, campground){
-    if(err){
-        console.log(err);
-    } else {
-        console.log("newly created campground");
-        console.log(campground);
-    }
-});
+// Campground.create({
+//     name: "Granite Hill", 
+//     image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"
+// }, function(err, campground){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log("newly created campground");
+//         console.log(campground);
+//     }
+// });
 
-let campgrounds = [
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-       ];
 
 app.get('/', function(req, res){
    res.render('landing'); 
 });
 
 app.get('/campgrounds', function(req, res){
-    res.render('campgrounds', {campgrounds: campgrounds});
+    // get all campgrounds from db
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render('campgrounds', {campgrounds: allCampgrounds});
+        }
+    });    
 });
 
 app.post('/campgrounds', function(req, res){
