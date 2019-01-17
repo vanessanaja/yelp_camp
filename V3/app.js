8b7ef653bfc97ed6bd6b8795789b0b8418be164f
Vanessa Naja <vcpn@yahoo.com>
2019-01-17 01:21:30 +0000

    update to show comments

--- V3/app.js
+++ V3/app.js
@@ -49,10 +49,11 @@ app.get('/campgrounds/new', function(req, res){
 
 app.get('/campgrounds/:id', function(req, res){
     //find campground with provided id
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
        if(err){
            console.log(err);
        } else {
+           console.log("found campground");
            // render show template with that campground
            res.render('show', {campground: foundCampground});
        }
