5ef440218157db3796e5b1532e733cc5cbb7c24e
Vanessa Naja <vcpn@yahoo.com>
2019-01-22 23:07:35 +0000

    set up passport

--- V6/app.js
+++ V6/app.js
@@ -6,7 +6,7 @@ const express = require("express"),
     Comment = require("./models/comment"),
     passport = require("passport"),
     LocalStrategy = require("passport-local"),
-    User = require("./models/user");
+    User = require("./models/user"),
     seedDB = require("./seeds");
     
     // seedDB();
@@ -14,6 +14,19 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
+
+app.use(require("express-session")({
+    secret: "Tiny is the best",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get('/', function(req, res){
    res.render('landing'); 
