97aba4721281a06f73e0948e16210ede0839718f
Vanessa Naja <vcpn@yahoo.com>
2019-01-23 18:05:25 +0000

    add logout route and update links to correct pages

--- V6/app.js
+++ V6/app.js
@@ -140,6 +140,11 @@ app.post('/login', passport.authenticate("local", {
     
 });
 
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/campgrounds');
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('Yelp Camp server has started');
 });
