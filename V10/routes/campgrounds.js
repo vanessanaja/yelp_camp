948b1833836fde27a4e51cea42269294962e84b4
Vanessa Naja <vcpn@yahoo.com>
2019-01-25 00:00:37 +0000

    campground edit put route and update form action and method

--- V10/routes/campgrounds.js
+++ V10/routes/campgrounds.js
@@ -60,6 +60,17 @@ router.get('/:id/edit', function(req, res){
     });
 });
 
+router.put('/:id', function(req, res){
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+             if(err){
+                 console.log(err);
+                 res.redirect('/campgrounds');
+             } else {
+                 res.redirect('/campgrounds/' + req.params.id);
+             }
+    });
+});
+
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
