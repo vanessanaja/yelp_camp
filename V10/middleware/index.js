4a18b0537a55061c4d12e72942961e4d7582a3f3
Vanessa Naja <vcpn@yahoo.com>
2019-01-28 19:39:57 +0000

    create middleware dir and file

---
+++ V10/middleware/index.js
@@ -0,0 +1,42 @@
+const middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next){
+    if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+            if(err){
+                console.log(err);
+                res.redirect('back');
+            } else {
+                if(foundCampground.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect('back');
+                }
+            }
+        });
+    } else {
+        res.redirect('back');
+    }
+};
+
+middlewareObj.checkCommentOwnership = function (req, res, next){
+    if(req.isAuthenticated()){
+            Comment.findById(req.params.comment_id, function(err, foundComment){
+                if(err){
+                    console.log(err);
+                    res.redirect('back');
+                } else {
+                    if(foundComment.author.id.equals(req.user._id)){
+                        next();
+                    } else {
+                        res.redirect('back');
+                    }
+                }
+            });
+    } else {
+        res.redirect('back');
+    }
+};
+
+
+module.exports = middlewareObj
