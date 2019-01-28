c176e5f9c15db3c3572f02517a99ba6a19231cfa
Vanessa Naja <vcpn@yahoo.com>
2019-01-28 20:49:36 +0000

    require connect-flash

--- V11/app.js
+++ V11/app.js
@@ -4,9 +4,10 @@ const   express = require("express"),
         mongoose = require("mongoose"),
         Campground = require("./models/campground"),
         Comment = require("./models/comment"),
-        passport = require("passport"),
+        flash = require("connect-flash"),
         LocalStrategy = require("passport-local"),
         methodOverride = require("method-override"),
+        passport = require("passport"),
         User = require("./models/user"),
         seedDB = require("./seeds");
     
@@ -21,6 +22,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride('_method'));
+app.use(flash());
 
 app.use(require("express-session")({
     secret: "Tiny is the best",
