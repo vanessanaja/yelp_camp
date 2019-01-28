86db323ff39fadb7e19581df9a8b4dd1d8d211f5
Vanessa Naja <vcpn@yahoo.com>
2019-01-28 21:06:39 +0000

    add flash message to login page when login is required

--- V11/routes/index.js
+++ V11/routes/index.js
@@ -25,7 +25,7 @@ router.post("/register", function(req, res){
 });
 
 router.get('/login', function(req, res){
-    res.render('login');
+    res.render('login', {message: req.flash('error')});
 });
 
 router.post('/login', passport.authenticate("local", {
