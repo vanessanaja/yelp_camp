a98e1b25647f7dcc5d309e50c7396d420b075938
Vanessa Naja <vcpn@yahoo.com>
2019-01-30 23:08:21 +0000

    update routes to reflect dynamic price

--- V12/routes/campgrounds.js
+++ V12/routes/campgrounds.js
@@ -16,13 +16,14 @@ router.get('/', function(req, res){
 
 router.post('/', middleware.isLoggedIn, function(req, res){
     const name = req.body.name; 
+    const price = req.body.price;
     const image = req.body.image; 
     const desc = req.body.description;
     const author = {
       id: req.user._id,
       username: req.user.username
     };
-    const newCampground = {name: name, image: image, description: desc, author: author};
+    const newCampground = {name: name, price: price, image: image, description: desc, author: author};
     
     //Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreatedCampground){
