18d1b4e1a363c821681c749e5315cea60af2073f
Vanessa Naja <vcpn@yahoo.com>
2019-02-01 00:23:54 +0000

    V12 Deployed

---
+++ V12Deployed/routes/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const User = require("../models/user");
+
+router.get('/', function(req, res){
+   res.render('landing'); 
+});
+
+router.get("/register", function(req, res){ 
+    res.render("register");
+});
+
+router.post("/register", function(req, res){
+    const newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+       if(err){
+           req.flash('error', err.message);
+           return res.redirect('/register');
+       }
+       passport.authenticate('local')(req, res, function(){
+           req.flash('success', 'Welcome to YelpCamp ' + user.username);
+           res.redirect('/campgrounds');
+       });
+   });
+});
+
+router.get('/login', function(req, res){
+    res.render('login');
+});
+
+router.post('/login', passport.authenticate("local", 
+    {
+      successRedirect: "/campgrounds",
+       failureRedirect: "/login"
+    }), function(req, res){
+});
+
+router.get('/logout', function(req, res){
+    req.logout();
+    req.flash('success', "Logged you out!");
+    res.redirect('/campgrounds');
+});
+
+module.exports = router;
