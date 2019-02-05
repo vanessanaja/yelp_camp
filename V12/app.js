f5afabcb09bd571b2f0d8988559a5aacbd51dec3
Vanessa Naja <vcpn@yahoo.com>
2019-02-05 18:55:40 +0000

    refactor app.use as arrow function

--- V12/app.js
+++ V12/app.js
@@ -36,7 +36,14 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
+// app.use(function(req, res, next){
+//   res.locals.currentUser = req.user;
+//   res.locals.error = req.flash('error');
+//   res.locals.success = req.flash('success');
+//   next();
+// });
+
+app.use((req, res, next)=>{
    res.locals.currentUser = req.user;
    res.locals.error = req.flash('error');
    res.locals.success = req.flash('success');
