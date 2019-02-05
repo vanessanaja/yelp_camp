41b644542b17d12e8ddeab0da28f8447af515bd8
Vanessa Naja <vcpn@yahoo.com>
2019-02-05 17:40:32 +0000

    use environment variable

--- V12Deployed/app.js
+++ V12Deployed/app.js
@@ -8,16 +8,15 @@ const   express = require("express"),
         LocalStrategy = require("passport-local"),
         methodOverride = require("method-override"),
         passport = require("passport"),
-        User = require("./models/user"),
-        seedDB = require("./seeds");
+        User = require("./models/user")
     
 const   commentRoutes = require("./routes/comments"),
         campgroundRoutes = require("./routes/campgrounds"),
         indexRoutes = require("./routes/index");
-    
-    
-    // seedDB();
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+        
+console.log(process.env.DATABASEURL);
+
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
@@ -36,11 +35,18 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   res.locals.error = req.flash('error');
-   res.locals.success = req.flash('success');
-   next();
+// app.use(function(req, res, next){
+//   res.locals.currentUser = req.user;
+//   res.locals.error = req.flash('error');
+//   res.locals.success = req.flash('success');
+//   next();
+// });
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
+  next();
 });
 
 app.use('/', indexRoutes);
