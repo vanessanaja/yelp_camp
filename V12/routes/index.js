ece3a39b5a73dea3ec2a2b43d6668aa0f56a0dba
Vanessa Naja <vcpn@yahoo.com>
2019-02-06 17:17:01 +0000

    refactor index routes with arrow functions

--- V12/routes/index.js
+++ V12/routes/index.js
@@ -3,29 +3,29 @@ const router = express.Router();
 const passport = require("passport");
 const User = require("../models/user");
 
-router.get('/', function(req, res){
+router.get('/', (req, res)=>{
    res.render('landing'); 
 });
 
-router.get("/register", function(req, res){ 
+router.get("/register", (req, res)=>{ 
     res.render("register");
 });
 
-router.post("/register", function(req, res){
+router.post("/register", (req, res)=>{
     const newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
+    User.register(newUser, req.body.password, (err, user)=>{
        if(err){
            req.flash('error', err.message);
            return res.redirect('/register');
        }
-       passport.authenticate('local')(req, res, function(){
+       passport.authenticate('local')(req, res, ()=>{
            req.flash('success', 'Welcome to YelpCamp ' + user.username);
            res.redirect('/campgrounds');
        });
    });
 });
 
-router.get('/login', function(req, res){
+router.get('/login', (req, res)=>{
     res.render('login');
 });
 
@@ -33,10 +33,10 @@ router.post('/login', passport.authenticate("local",
     {
       successRedirect: "/campgrounds",
        failureRedirect: "/login"
-    }), function(req, res){
+    }), (req, res)=>{
 });
 
-router.get('/logout', function(req, res){
+router.get('/logout', (req, res)=>{
     req.logout();
     req.flash('success', "Logged you out!");
     res.redirect('/campgrounds');
